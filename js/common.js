015d342fd577a56eaf4667c1c070f60c76e0731a
RahulChhabra <chh_rahul@live.com>
2018-08-21 12:32:14 +0530

    3

---
+++ js/common.js
@@ -0,0 +1,40 @@
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+function onDeviceReady() {
+    var fileUrl = 'http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf';
+  var fileName = 'pdf-sample.pdf';
+	var fileTransfer = new FileTransfer();
+	var uri = encodeURI(fileUrl);
+  //var fileUrl = cordova.file.documentsDirectory;
+  var fileUrl = cordova.file.externalRootDirectory;
+ 
+  alert(fileUrl)
+		fileTransfer.download(
+			uri,
+			fileUrl + "/" + fileName,
+			function (entry) {
+			entry.file(function (file) {
+				    alert(entry.toURL());
+            alert(file.type);
+                //openFile(entry.toURL(), file.type);
+				
+			},
+				function (fail) {
+				alert('failed')
+		});
+    },
+			function (error) {
+      alert('error')
+		},
+			true
+      );
+}
+
+function show_more_menu() {
+    
+	
+  
+	
+
+}
--- js/index.js
+++ js/index.js
@@ -44,3 +44,36 @@ var app = {
 };
 
 app.initialize();
+
+function downloadDoc() {
+    
+	var fileUrl = 'http://unec.edu.az/application/uploads/2014/12/pdf-sample.pdf';
+  var fileName = 'pdf-sample.pdf';
+	var fileTransfer = new FileTransfer();
+	var uri = encodeURI(fileUrl);
+  //var fileUrl = cordova.file.documentsDirectory;
+  var fileUrl = cordova.file.externalCacheDirectory;
+ 
+  alert(fileUrl)
+		fileTransfer.download(
+			uri,
+			fileUrl + "/" + fileName,
+			function (entry) {
+			entry.file(function (file) {
+				    alert(entry.toURL());
+            alert(file.type);
+                //openFile(entry.toURL(), file.type);
+				
+			},
+				function (fail) {
+				alert('failed')
+		});
+    },
+			function (error) {
+      alert('error')
+		},
+			true
+      );
+	
+
+}
